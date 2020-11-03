b8d33d420d71d7e6b6ab19d8c79bf5932f03edbe
QW3RTZUI0P <martinjurk102@gmail.com>
2020-11-03 20:41:17 +0000

    0.1.1
    now the skill is working at its core

--- __init__.py
+++ __init__.py
@@ -1,67 +1,68 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler
 import requests
 import json
 
 # important values for the skill to function
 sonos_server_ip = "192.168.1.220"
 room = "Küche"
+url = "http://" + sonos_server_ip + ":5005/" + room + "/"
+
+# search algorithms
+# function for searching a song on Apple Music
+def search_song_applemusic(title="", interpreter=""):
+    # this is the url that returns a json file with the search results from the iTunes Search API (https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/)
+    urlInFunction = "https://itunes.apple.com/search?term=" + title +  "&country=de&media=music&entity=song&attribute=songTerm&artistTerm=" + interpreter
+    response = requests.get(urlInFunction)
+    resultsJson = response.json()
+    bestResult = resultsJson["results"][0]
+    trackId = bestResult["trackId"]
+    return trackId
+# function for searching an album on Apple Music
+def search_album_applemusic(title="", interpreter=""):
+    urlInFunction = "https://itunes.apple.com/search?term=" + title + "&country=de&media=music&entity=album&attribute=albumTerm&artistTerm=" + interpreter
+    response = requests.get(urlInFunction)
+    resultsJson = response.json()
+    bestResult = results["results"][0]
+    collectionId = bestResult["collectionId"]
+    return collectionId
+
 
 class SonosMusicController(MycroftSkill):
-    
-    url = "http://" + sonos_server_ip + ":5005/" + room + "/"
 
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    # search algorithms
-    # function for searching a song on Apple Music
-    def search_song_applemusic(title = "", interpreter = ""):
-	# this is the url that returns a json file with the search results from the iTunes Search API (https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/)
-	url = "https://itunes.apple.com/search?term=" + title +  "&country=de&media=music&entity=song&attribute=songTerm&artistTerm=" + interpreter
-	response = requests.get(url)
-        resultsJson = response.json()
-	bestResult = jsonAsValue["results"][0]
-        trackId = bestResult["trackId"]
-	return trackId
-
-    # function for searching an album on Apple Music
-    def search_album_applemusic(title = "", interpreter = ""):
-	    url = "https://itunes.apple.com/search?term=" + title +  "&country=de&media=music&entity=album&attribute=albumTerm&artistTerm=" +interpreter
-        response = requests.get(url)
-        resultsJson = response.json()
-	    bestResult = jsonAsValue["results"][0]
-	    collectionId = bestResult["collectionId"]
-	    return collectionId
-
+   
     # function for searching the essentials playlist of an interpreter
-    def search_essentials_playlist:
+    # def search_essentials_playlist:
 	# this is currently not working because the iTunes Search API doesn't allow searches for playlists
 
-
     # General controls for Sonos
-    @intent_file_handler('pause.intent'):
-    def pause():
-	    requests.get(url + "pause")
+    @intent_handler('pause.intent')
+    def pause(self, message):
+        requests.get(url + "pause")
 
-    @intent_file_handler('resume.intent'):
-    def resume():
-	    requests.get(url + "play")
-
-    @intent_file_handler('louder.intent'):
-    def louder():
+    @intent_handler('resume.intent')
+    def resume(self, message):
+        self.log.info(url + "play")
+        self.speak_dialog('resume.dialog')
+        requests.get(url + "play")
+        
+    @intent_handler('louder.intent')
+    def louder(self, message):
         requests.get(url + "volume/+10")
-	
-    @intent_file_handler('quieter.intent'):
-    def quieter():
-        requests.get(url + "volume/-10"
+
+    @intent_handler('quieter.intent')
+    def quieter(self, message):
+        requests.get(url + "volume/-10")
 
 
     # playing music on Sonos
-    @intent_file_handler('play.intent'):
-    def play():
-        trackId = search_song_applemusic(message.data.get('title'))
-        requests.get(url + "applemusic/now/song:" + str(trackId)
+    @intent_handler('play.intent')
+    def play_music(self, message):
+        print("title: " + message.data.get('title') + message.data.get('interpreter'))
+        trackId=search_song_applemusic(message.data.get('title'), message.data.get('interpreter'))
+        requests.get(url + "applemusic/now/song:" + str(trackId))
 
 def create_skill():
     return SonosMusicController()
-
